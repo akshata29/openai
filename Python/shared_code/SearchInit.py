ab2c3b5c4116a03f56fe80902f2cab2b1f2fa77a
Ashish Talati <astalati@microsoft.com>
2023-02-01 22:30:54 -0600

    Summarize

--- Python/shared_code/SearchInit.py
+++ Python/shared_code/SearchInit.py
@@ -8,23 +8,19 @@ import logging
 import datetime
 from json import JSONEncoder
 import azure.functions as func
-from azure.ai.formrecognizer import FormRecognizerClient
+from azure.search.documents import SearchClient
 from azure.core.credentials import AzureKeyCredential
-from azure.ai.formrecognizer import DocumentAnalysisClient
+
+
+service_endpoint = os.getenv("AZURE_SEARCH_SERVICE_ENDPOINT")
+index_name = os.getenv("AZURE_SEARCH_INDEX_NAME")
+key = os.getenv("AZURE_SEARCH_API_KEY")
 
 class DateTimeEncoder(JSONEncoder):
         #Override the default method
         def default(self, obj):
             if isinstance(obj, (datetime.date, datetime.datetime)):
                 return obj.isoformat()
-
-endpoint = os.environ["FORMS_RECOGNIZER_ENDPOINT"]
-key = os.environ["FORMS_RECOGNIZER_KEY"]
-
-form_recognizer_client = FormRecognizerClient(endpoint, AzureKeyCredential(key))
-document_analysis_client = DocumentAnalysisClient(
-    endpoint=endpoint, credential=AzureKeyCredential(key)
-)
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     logging.info(f'{context.function_name} HTTP trigger function processed a request.')
@@ -58,6 +54,21 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
              status_code=400
         )
 
+def cogsearch_query(search_text_string, numberofresults):
+    
+    from azure.core.credentials import AzureKeyCredential
+    from azure.search.documents import SearchClient
+
+    search_client = SearchClient(service_endpoint, index_name, AzureKeyCredential(key))
+    results = search_client.search(search_text=search_text_string, include_total_count =1, top=numberofresults)
+    print("records containing the string")
+    for result in results:
+            print(json.dumps(result))
+    return results
+
+
+
+
 def compose_response(json_data):
     values = json.loads(json_data)['values']
     # Prepare the Output before the loop
@@ -71,221 +82,23 @@ def compose_response(json_data):
     
     return json.dumps(results, ensure_ascii=False, cls=DateTimeEncoder)
 
-#
-# Extract Tables from Results
-#
-def extract_tables(result):
 
-    tables = []
-    for table_idx, table in enumerate(result.tables):       
-        print(
-            "Table # {} has {} rows and {} columns".format(
-                table_idx, table.row_count, table.column_count
-            )
-        )
-
-        for region in table.bounding_regions:
-            print(
-                "Table # {} location on page: {} is {}".format(
-                    table_idx,
-                    region.page_number,
-                    region.polygon,
-                )
-            )
-
-        cells = []
-        for cell in table.cells:
-            print(
-                "...Cell[{}][{}] has content '{}'".format(
-                    cell.row_index,
-                    cell.column_index,
-                    cell.content,
-                )
-            )
-            cells.append(
-                {
-                    "text": cell.content,
-                    "rowIndex": cell.row_index,
-                    "rowSpan": cell.row_span,
-                    "colIndex": cell.column_index,
-                    "colSpan": cell.column_span,
-                    "is_header": (cell.kind=='columnHeader')
-                }
-            )
-            for region in cell.bounding_regions:
-                print(
-                    "...content on page {} is within bounding polygon '{}'\n".format(
-                        region.page_number,
-                        region.polygon,
-                    )
-                )
-
-        tables.append(
-            {
-                "row_count": table.row_count,
-                "column_count": table.column_count,
-                "cells": cells
-            }
-        )
-    return tables
-
-#
-# Extract Pages (Read)
-#
-def extract_pages(result):
-    for page in result.pages:
-        print("----Analyzing document from page #{}----".format(page.page_number))
-        print(
-            "Page has width: {} and height: {}, measured with unit: {}".format(
-                page.width, page.height, page.unit
-            )
-        )
-
-        for line_idx, line in enumerate(page.lines):
-            words = line.get_words()
-            print(
-                "...Line # {} has {} words and text '{}' within bounding polygon '{}'".format(
-                    line_idx,
-                    len(words),
-                    line.content,
-                    line.polygon,
-                )
-            )
-
-            for word in words:
-                print(
-                    "......Word '{}' has a confidence of {}".format(
-                        word.content, word.confidence
-                    )
-                )
-
-        for selection_mark in page.selection_marks:
-            print(
-                "...Selection mark is '{}' within bounding polygon '{}' and has a confidence of {}".format(
-                    selection_mark.state,
-                    selection_mark.polygon,
-                    selection_mark.confidence,
-                )
-            )
-
-#
-# Extract Paragraphs (Layout)
-#
-def extract_paragraphs(result):
-    paragraphs=[]
-    for paragraph in result.paragraphs:
-        paragraphs.append(paragraph.content)
-
-    return paragraphs
-
-#
-# Extract KV pairs
-#
-def extract_kv(result):
-    kvs=[]
-    for kv_pair in result.key_value_pairs:
-        if kv_pair.key:
-            print(
-                    "Key '{}' found within '{}' bounding regions".format(
-                        kv_pair.key.content,
-                        kv_pair.key.bounding_regions,
-                    )
-                )
-        if kv_pair.value:
-            print(
-                    "Value '{}' found within '{}' bounding regions\n".format(
-                        kv_pair.value.content,
-                        kv_pair.value.bounding_regions,
-                    )
-                )
-
-        if kv_pair.key and kv_pair.value:
-            kvs.append({
-                "key":kv_pair.key.content,
-                "value":kv_pair.value.content
-            })
-        else:
-            kvs.append({
-                "key":kv_pair.key.content,
-                "value":''
-            })
-
-    return kvs
-
-## Perform an operation on a record
 def transform_value(value):
-    try:
-        recordId = value['recordId']
-    except AssertionError  as error:
-        return None
+      
     # Validate the inputs
     try:
-        assert ('data' in value), "'data' field is required."
-        data = value['data']   
-        print(data)
-
-        form_url = data["formUrl"]  + data["formSasToken"]   
-        print(form_url)
-        # poller = form_recognizer_client.begin_recognize_content_from_url(form_url)
-        # pages = poller.result()  
-
-        poller = document_analysis_client.begin_analyze_document_from_url("prebuilt-document", form_url)
-        result = poller.result()
-
-        # Extract Pages
-        # pages = extract_pages(result)
-
-        # Extract Paragraphs
-        paragraphs = extract_paragraphs(result)
-
-        # Extract Tables
-        tables = extract_tables(result)
-        
-        # Extract Key Value Pairs
-        kvs = extract_kv(result)
-
-        # Extract Styles
-        for style in result.styles:
-            if style.is_handwritten:
-                print("Document contains handwritten content: ")
-                print(",".join([result.content[span.offset:span.offset + span.length] for span in style.spans]))
-
+        assert ('searchQuery' in value), "'searchQuery' field is required."
+        searchQuery = value['searchquery']   
+        print(searchQuery)
     except AssertionError  as error:
-        return (
-            {
-            "recordId": recordId,
-            "data": {
-                "tables": [],
-                "tables_count": 0,
-                "paragraphs": [],
-                "paragraphs_count": 0,
-                "kvs": [],
-                "kvs_count": 0
-            },
-            "warnings": [ { "message": "Error:" + error.args[0] }   ]       
-            })
-    except Exception as error:
-        return (
-            {
-            "recordId": recordId,
-            "data": {
-                "tables": [],
-                "tables_count": 0,
-                "paragraphs": [],
-                "paragraphs_count": 0,
-                "kvs": [],
-                "kvs_count": 0
-            },
-            "warnings": [ { "message": "Error:" + str(error) }   ]       
-            })
-    return ({
-            "recordId": recordId,
-            "data": {
-                "tables": tables,
-                "tables_count": len(tables),
-                "paragraphs": paragraphs,
-                "paragraphs_count": len(paragraphs),
-                "kvs": kvs,
-                "kvs_count": len(kvs)
-            }
-            })
+        return None
+
+      # Validate the inputs
+    try:
+        assert ('seachItemNumbers' in value), "'seachItemNumbers' field is will be defaulted to 10."
+        seachItemNumbers = value['seachItemNumbers']   
+        print(seachItemNumbers)
+    except AssertionError  as warning:
+        seachItemNumbers = 10
+    search_result = cogsearch_query(searchQuery, seachItemNumbers)
+    return search_result
--- Python/shared_code/SummarizeInit.py
+++ Python/shared_code/SummarizeInit.py
@@ -5,38 +5,36 @@
 import logging
 import json
 import azure.functions as func
-from PIL import Image, ImageSequence
-import PIL
-import base64
-from io import BytesIO
-import io
-import math
+import openai
+import re
+import requests
+import sys
 import os
+import pandas as pd
+import numpy as np
+from openai.embeddings_utils import get_embedding, cosine_similarity
 
-# https://stackoverflow.com/questions/51152059/pillow-in-python-wont-let-me-open-image-exceeds-limit
-# PIL.Image.MAX_IMAGE_PIXELS = 933120000
-PIL.Image.MAX_IMAGE_PIXELS = None
+opanaiKey = os.environ['OpenAiKey']
+openaiEndpoint = os.environ['OpenAiEndPoint']
+openAiVersion = os.environ['OpenAiVersion']
 
-# Azure Computer Vision OCR Minimum image size limits
-MIN_WIDTH=50
-MIN_HEIGHT=50
-#
-# Example: Azure Computer Vision OCR has a maximum image size limit of 10000x10000. 
-#
-MAX_WIDTH=10000
-MAX_HEIGHT=10000
-MAX_IMAGE_SIZE=50*1024*1024
-IMAGE_RATIO_THRESHOLD=3
-IMAGE_CROP_OVERLAP=50
+#Splits text after sentences ending in a period. Combines n sentences per chunk.
+def splitter(n, s):
+    pieces = s.split(". ")
+    list_out = [" ".join(pieces[i:i+n]) for i in range(0, len(pieces), n)]
+    return list_out
 
-DEFAULT_MIN_IMAGE_MODE = 'RGBA' # for color image “L” (luminance) for greyscale images, “RGB” for true color images, and “CMYK” for pre-press images.
-DEFAULT_MIN_IMAGE_SIZE = (MIN_WIDTH, MIN_HEIGHT)
-DEFAULT_MIN_IMAGE_COLOR = (255, 255, 255)
-
-# os.environ.get('image_min_width',MIN_WIDTH)
-# os.environ.get('image_min_height',MIN_HEIGHT)
-# os.environ.get('image_max_width',MAX_WIDTH)
-# os.environ.get('image_max_height',MAX_HEIGHT)
+# Perform light data cleaning (removing redudant whitespace and cleaning up punctuation)
+def normalize_text(s, sep_token = " \n "):
+    s = re.sub(r'\s+',  ' ', s).strip()
+    s = re.sub(r". ,","",s)
+    # remove all instances of multiple spaces
+    s = s.replace("..",".")
+    s = s.replace(". .",".")
+    s = s.replace("\n", "")
+    s = s.strip()
+    
+    return s
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     logging.info(f'{context.function_name} HTTP trigger function processed a request.')
@@ -49,6 +47,10 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
                 f"function {context.function_name} has been reached")
 
     try:
+        userQuery = req.params.get('userQuery')
+        totalDocs = req.params.get('totalDocs')
+        modelName = req.params.get('modelName')
+        logging.info("Input parameters : " + userQuery + " " + totalDocs + " " + modelName)
         body = json.dumps(req.get_json())
     except ValueError:
         return func.HttpResponse(
@@ -57,7 +59,7 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
         )
     
     if body:
-        result = compose_response(body)
+        result = compose_response(userQuery, totalDocs, modelName, body)
         return func.HttpResponse(result, mimetype="application/json")
     else:
         return func.HttpResponse(
@@ -65,7 +67,7 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
              status_code=400
         )
 
-def compose_response(json_data):
+def compose_response(userQuery, totalDocs, modelName, json_data):
     values = json.loads(json_data)['values']
     
     # Prepare the Output before the loop
@@ -73,37 +75,13 @@ def compose_response(json_data):
     results["values"] = []
 
     for value in values:
-        output_record = transform_value(value)
+        output_record = transform_value(value, userQuery, totalDocs, modelName)
         if output_record != None:
             results["values"].append(output_record)
     return json.dumps(results, ensure_ascii=False)        
-#
-# PIL Conversion to PNG - Greyscale 
-#
-def img_to_base64_str_with_conversion(img):
-    buffered = BytesIO()
-    img = img.convert('L')
-    img.save(buffered, format="PNG", optimize=True)
-    buffered.seek(0)
-    img_byte = buffered.getvalue()
-    # img_file_size_png = buffered.tell()
-    img_file_size_png = len(img_byte)
-    return img_file_size_png, base64.b64encode(img_byte).decode()
-
-def img_to_base64_str(img, target_format = "PNG"):
-    if target_format in ["JPEG","JPG"]:
-        if img.mode != 'RGB':
-            img = img.convert('RGB')
-    buffered = BytesIO()
-    img.save(buffered, format=target_format, optimize=True)
-    buffered.seek(0)
-    img_byte = buffered.getvalue()
-    img_file_size_png = len(img_byte)
-    return img_file_size_png, base64.b64encode(img_byte).decode()
-
 
 ## Perform an operation on a record
-def transform_value(record):
+def transform_value(record, userQuery, totalDocs, modelName):
     try:
         recordId = record['recordId']
     except AssertionError  as error:
@@ -111,207 +89,9 @@ def transform_value(record):
 
     # Validate the inputs
     try:
-        document = {}
-        document['recordId'] = recordId
-        document['data'] = {}
-
-        images=[]
-
-        # Original Image Size
-        size=(MAX_WIDTH,MAX_HEIGHT)
-        image_string=record['data']["file_data"]["data"]
-
-        contentType=record['data']["file_data"]['contentType']
-
-        image = io.BytesIO(base64.b64decode(image_string))
-
-        orig_image = Image.open(image)
-
-        # Image Metadata
-        #
-        (imagew,imageh) = orig_image.size
-
-        # Calculate Image Size Ratio
-        if imagew>imageh:
-            image_ratio = imagew/imageh
-        else:
-            image_ratio = imageh/imagew
-
-        # Metadata handling
-        document['data']['image_metadata']={}
-        document['data']['image_metadata']['width']=imagew
-        document['data']['image_metadata']['height']=imageh
-        document['data']['image_metadata']['ratio']=image_ratio
-
-        # Thumbnail of the original image
-        tbimg = orig_image.copy()
-        tbimg.thumbnail((400,400))
-        _, document['data']['image_metadata']['thumbnail_medium'] = img_to_base64_str(tbimg,"JPEG")
-        tbimg.thumbnail((100,100))
-        _, document['data']['image_metadata']['thumbnail_small'] = img_to_base64_str(tbimg,"JPEG")
-        del tbimg
-        logging.info('Image small & medium thumbnails created.')
-
-        # Normalization
-        #
-        # Iterate through all images including frames (TIFF)
-        for j, read_image in enumerate(ImageSequence.Iterator(orig_image)):
-            # For each image (TIFF multi page included) 
-            (imagew,imageh) = read_image.size
-
-            # Calculate Image Size Ratio
-            if imagew>imageh:
-                image_ratio = imagew/imageh
-            else:
-                image_ratio = imageh/imagew
-
-            # Deal with small images
-            if imagew<MIN_WIDTH or imageh<MIN_HEIGHT:
-                logging.info(f'Small Image W {imagew} H {imageh} Size Ratio {image_ratio}')
-                # paste the crop image into a minimal size image.
-                minimg = Image.new(DEFAULT_MIN_IMAGE_MODE, (max(imagew,MIN_WIDTH),max(imageh,MIN_HEIGHT)), DEFAULT_MIN_IMAGE_COLOR)
-                minimg.paste(read_image)
-                image = {}
-                image['minsize'] = True
-                image['$type'] = "file"
-                image['originalWidth'] = imagew
-                image['originalHeight'] = imageh
-                image['originalSizeRatio'] = image_ratio
-                image['width'] = max(imagew,MIN_WIDTH)
-                image['height'] = max(imageh,MIN_HEIGHT)
-                if "contentType" in record['data']["file_data"]:
-                    image['contentType'] = record['data']["file_data"]["contentType"]
-                image['rotationFromOriginal']=0
-                if "url" in record['data']["file_data"]:
-                    image['url']=record['data']["file_data"]['url']
-                image['size'], image['data'] = img_to_base64_str(minimg)
-                
-                images.append(image)
-
-            # Oversized images
-            elif imagew>MAX_WIDTH or imageh>MAX_HEIGHT or image_ratio > IMAGE_RATIO_THRESHOLD:
-                logging.info(f'Frame {j} Image outside Max Width or Height or Size Ratio.{(imagew,imageh,image_ratio)}')
-                # Crop is required here
-                wcrops = math.floor(imagew/MAX_WIDTH)
-                if imagew % MAX_WIDTH != 0:
-                    wcrops+=1
-                hcrops = math.floor(imageh/MAX_HEIGHT)
-                if imageh % MAX_HEIGHT != 0:
-                    hcrops+=1
-
-                # Let's now look at the image ratio of each tuple w,h 
-                cropslist=[]
-                for wkey in range(wcrops):
-                    cropwidth=min(MAX_WIDTH,imagew-(wkey*MAX_WIDTH))
-                    for hkey in range(hcrops):
-                        cropheight=min(MAX_HEIGHT,imageh-(hkey*MAX_HEIGHT))
-
-                        if cropwidth>cropheight:
-                            ratio = cropwidth/cropheight
-                        else:
-                            ratio = cropheight/cropwidth
-
-                        if ratio==1 or ratio>IMAGE_RATIO_THRESHOLD:
-                            # Now repeat similar crop based on ratio this time
-
-                            if cropwidth==cropheight:
-                                cropslist.append((wkey*MAX_WIDTH,hkey*MAX_HEIGHT,(wkey*MAX_WIDTH)+cropwidth,(hkey*MAX_HEIGHT)+cropheight))
-                            elif cropwidth>cropheight:
-                                # crop on width
-                                cropslist.append((wkey*MAX_WIDTH,hkey*MAX_HEIGHT,(wkey*MAX_WIDTH)+cropwidth,(hkey*MAX_HEIGHT)+ min(math.ceil(cropwidth/IMAGE_RATIO_THRESHOLD),MAX_HEIGHT)))
-
-                                # denominator=(cropheight*IMAGE_RATIO_THRESHOLD)
-                                # cropsratio = math.floor(cropwidth/denominator)
-                                # if cropwidth % denominator != 0:
-                                #     cropsratio+=1
-                                # for wkeyr in range(cropsratio):
-                                #     # we move the width n times
-                                #     w1=denominator*wkeyr
-                                #     w2=min(cropwidth,denominator*(wkeyr+1))
-                                #     if w1!=w2:
-                                #         cropslist.append(((wkey*MAX_WIDTH)+w1,hkey*MAX_HEIGHT,(wkey*MAX_WIDTH)+w2,(hkey*MAX_HEIGHT)+cropheight))
-                                #     else:
-                                #         logging.info(f'Image crop has same width. Skipping {w1} {w2}')
-                            else:
-                                # Crop on height
-                                cropslist.append((wkey*MAX_WIDTH,hkey*MAX_HEIGHT,(wkey*MAX_WIDTH)+min(math.ceil(cropheight/IMAGE_RATIO_THRESHOLD),MAX_WIDTH),(hkey*MAX_HEIGHT)+cropheight))
-
-                                # denominator=(cropwidth*IMAGE_RATIO_THRESHOLD)
-                                # cropsratio = math.floor(cropheight/denominator)
-                                # # if cropheight % denominator < MIN_HEIGHT:
-                                # #     merge_last_entry=True
-                                # # else:
-                                # #     merge_last_entry=False
-                                # if cropheight % denominator != 0:
-                                #     cropsratio+=1
-                                # for wkeyr in range(cropsratio):
-                                #     # we move the height n times
-                                #     h1=denominator*wkeyr
-                                #     h2=min(cropheight,denominator*(wkeyr+1))
-                                #     if h1!=h2:
-                                #         cropslist.append((wkey*MAX_WIDTH,(hkey*MAX_HEIGHT)+h1,(wkey*MAX_WIDTH)+cropwidth,(hkey*MAX_HEIGHT)+h2))
-                                #     else:
-                                #         logging.info(f'Image crop has same height. Skipping {h1} {h2}')
-                        else:
-                            cropslist.append((wkey*MAX_WIDTH,hkey*MAX_HEIGHT,(wkey*MAX_WIDTH)+cropwidth,(hkey*MAX_HEIGHT)+cropheight))
-
-                logging.info(f'Image crops list {cropslist}')
-
-                for (idx,boundingbox) in enumerate(cropslist):
-                    logging.info(f'Image normalized {idx} {boundingbox}')
-                    image = {}
-                    image['$type'] = "file"
-                    image['originalWidth'] = imagew
-                    image['originalHeight'] = imageh
-                    image['originalSizeRatio'] = image_ratio
-
-                    cropwidth=boundingbox[2]-boundingbox[0]
-                    cropheight=boundingbox[3]-boundingbox[1]
-
-                    # Documentation crop((x, y, x + width, y + height))
-                    crop_image = read_image.crop(box=boundingbox)
-
-                    if cropwidth<MIN_WIDTH or cropheight<MIN_HEIGHT:
-                        # paste the crop image into a minimal size image.
-                        minimg = Image.new(DEFAULT_MIN_IMAGE_MODE, (max(cropwidth,MIN_WIDTH),max(cropheight,MIN_HEIGHT)), DEFAULT_MIN_IMAGE_COLOR)
-                        minimg.paste(crop_image.resize((max(cropwidth,MIN_WIDTH),max(cropheight,MIN_HEIGHT))), (0,0))
-                        crop_image=minimg
-                        image['minsize'] = True
-
-                    image['frame'] = j
-                    image['idx'] = idx
-                    image['width'] = crop_image.size[0]
-                    image['height'] = crop_image.size[1]
-                    image['contentType'] = "image/jpeg"
-                    image['rotationFromOriginal']=0
-                    image['url']=record['data']["file_data"]['url']
-                    image['size'], image['data'] = img_to_base64_str(crop_image,"JPEG")
-
-                    images.append(image)
-            else:
-                logging.info(f'Frame {j} Image W {imagew} H {imageh} Size Ratio {image_ratio}')
-                image = {}
-                image['$type'] = "file"
-                image['frame'] = j
-                image['originalWidth'] = imagew
-                image['originalHeight'] = imageh
-                image['originalSizeRatio'] = image_ratio
-                image['width'] = imagew
-                image['height'] = imageh
-                if "contentType" in record['data']["file_data"]:
-                    image['contentType'] = record['data']["file_data"]["contentType"]
-                image['rotationFromOriginal']=0
-                if "url" in record['data']["file_data"]:
-                    image['url']=record['data']["file_data"]['url']
-                image['size'], image['data'] = img_to_base64_str(read_image)
-                
-                images.append(image)
-
-        document['data']['normalized_images'] = images
-
-        # This should be a configuration
-        if contentType in ['image/tiff','image/tiff-fx']:
-            document['data']['image_metadata']['image_data'] = images[0]['data']
+        assert ('data' in record), "'data' field is required."
+        data = record['data']        
+        assert ('text' in data), "'text' field is required in 'data' object."   
 
     except KeyError as error:
         return (
@@ -332,4 +112,62 @@ def transform_value(record):
             "errors": [ { "message": "SystemError:" + error.args[0] }   ]       
             })
 
-    return (document)
+    try:
+        openai.api_type = "azure"
+        openai.api_key = opanaiKey
+        openai.api_base = openaiEndpoint
+        openai.api_version = openAiVersion
+
+        # Getting the items from the values/data/text
+        myStringList = []
+        myStringList = data['text']
+
+        # Cleaning the list, removing duplicates
+        myStringList = list(dict.fromkeys(myStringList))
+
+        '''
+        Designing a prompt that will show and tell GPT-3 how to proceed. 
+        + Providing an instruction to summarize the text about the general topic (prefix)
+        + Providing quality data for the chunks to summarize and specifically mentioning they are the text provided (context + context primer)
+        + Providing a space for GPT-3 to fill in the summary to follow the format (suffix)
+        '''
+
+        #prompt_i = userQuery + '\n\n\Text:\n' + ' '.join([normalize_text(myStringList)])
+        prompt_i = userQuery + '\m\nText:\n' + ' '.join(myStringList)
+        #logging.info(prompt_i)
+
+        # for item in myStringList:
+        #     logging.info(prompt_i)
+        #     logging.info(item)
+        #     prompt_i = prompt_i + '\n\n\Text:\n' + " ".join([normalize_text(item)])
+
+        prompt = "".join([prompt_i, '\n\n Summary:\n'])
+
+        #logging.info("Prompt ", prompt)
+
+        # Using a temperature a low temperature to limit the creativity in the response. 
+        response = openai.Completion.create(
+                engine= modelName,
+                prompt = prompt,
+                temperature = 0.4,
+                max_tokens = 500,
+                top_p = 1.0,
+                frequency_penalty=0.5,
+                presence_penalty = 0.5,
+                best_of = 1
+            )
+
+        summaryResponse = response.choices[0].text
+        return ({
+            "recordId": recordId,
+            "data": {
+                "text": summaryResponse
+                    }
+            })
+
+    except:
+        return (
+            {
+            "recordId": recordId,
+            "errors": [ { "message": "Could not complete operation for record." }   ]
+            })
